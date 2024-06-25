49995efc0e35d4ab005ae0305dc1ffa1c3154ff1
felixhard <felixhard32@gmail.com>
2024-06-25 20:19:08 +0200

    initializing repo

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen felx-col bg-[#F1F1F1]">
+      <h1 className="">Portfolio</h1>
+    </main>
+  );
+}
