bdb86a5436405b2ba12e79ef06b55688c749f509
felixhard <felixhard32@gmail.com>
2024-07-07 18:38:38 +0200

    Implemented a section for clients to be able to contact me via a form that sends the content in an email.

---
+++ app/api/send/route.js
@@ -0,0 +1,32 @@
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+
+export async function POST(req, res) {
+  const { body } = await req.json();
+  const { email, subject, message } = body;
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Felix <felixhard32@gmail.com>",
+      to: ["felixhard32@gmail.com"],
+      subject: "Hello world",
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting me!</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
--- app/page.js
+++ app/page.js
@@ -3,17 +3,20 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#1E1E1E]">
-        <Navbar/>
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
-        <div></div>
+        <EmailSection />
       </div>
+      <Footer />
     </main>
   );
 }
